2f39e27ed35aeb8acfd639dc9012312806cadeb0
EirBec <eirbec.kodehode@gmail.com>
2023-09-11 13:40:01 +0200

    Buissness card first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+import Chief from "./assets/masterchief.png"
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <div>
+          <img src={Chief} className='' height='200px'/>
+        </div>
+        <div>
+        
+        </div>  
+      </div>  
+    </>
+  )
+}
+
+export default App
